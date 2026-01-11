8b6320372047d54a925c36a73eb5b67bcd574f91
Ashley Hawkins <awhawkins@proton.me>
2026-01-11 22:49:03 +0000

    Format and reorganise

--- src/type_checker.rs
+++ src/type_checker.rs
@@ -343,101 +343,123 @@ impl<'a> ValidateTypes<'a> for Spanned<Expr<'a>> {
 
 pub fn process_type_errors<'a>(src: &'a str, file_name: String, type_errors: &[TypeError<'a>]) {
     for e in type_errors {
-        let report = Report::build(
-                    ReportKind::Error,
-                    (
-                        file_name.clone(),
-                        e.origin_expr.span.start.bytes..e.origin_expr.span.end.bytes,
-                    ),
-                )
-                .with_message(format!(
-                    "Type mismatch: expected {}, found {}",
-                    e.expected, e.found
-                ))
-                .with_labels([
-                    Label::new((
-                        file_name.clone(),
-                        e.origin_expr.span.start.bytes..e.origin_expr.span.end.bytes,
-                    ))
-                    .with_message(format!("expected type {}, but this expression results in type {}", e.expected, e.found))
-                    .with_color(Color::Red),
-                    Label::new((
-                        file_name.clone(),
-                        match &e.context {
-                            TypeErrorContext::SubExprOf(context) => match &context.inner {
-                                Expr::BinaryOp { op, .. } => op.span.start.bytes..op.span.end.bytes,
-                                Expr::ArrayAccess { left, right } => right.span.start.bytes..right.span.end.bytes,
-                                _ => context.span.start.bytes..context.span.end.bytes,
-                            }
-                            TypeErrorContext::IfStatementCond(span) => span.start.bytes..span.end.bytes,
-                            TypeErrorContext::WhileStatementCond(span) => span.start.bytes..span.end.bytes,
-                            TypeErrorContext::ForStatementRange(span) => span.start.bytes..span.end.bytes,
-                            TypeErrorContext::Other => e.origin_expr.span.start.bytes..e.origin_expr.span.end.bytes,
-                        },
-                    ))
-                    .with_color(Color::Yellow)
-                    .with_message(match &e.context {
-                        TypeErrorContext::SubExprOf(context) => match &context.inner {
-                        Expr::BinaryOp { op, .. } => {
-                            let op_name = op.inner.to_string();
+        let hint_span = (
+            file_name.clone(),
+            match &e.context {
+                TypeErrorContext::SubExprOf(context) => match &context.inner {
+                    Expr::BinaryOp { op, .. } => op.span.start.bytes..op.span.end.bytes,
+                    Expr::ArrayAccess { left, right } => {
+                        right.span.start.bytes..right.span.end.bytes
+                    }
+                    _ => context.span.start.bytes..context.span.end.bytes,
+                },
+                TypeErrorContext::IfStatementCond(span) => span.start.bytes..span.end.bytes,
+                TypeErrorContext::WhileStatementCond(span) => span.start.bytes..span.end.bytes,
+                TypeErrorContext::ForStatementRange(span) => span.start.bytes..span.end.bytes,
+                TypeErrorContext::Other => {
+                    e.origin_expr.span.start.bytes..e.origin_expr.span.end.bytes
+                }
+            },
+        );
 
+        let hint_message = match &e.context {
+            TypeErrorContext::SubExprOf(context) => match &context.inner {
+                Expr::BinaryOp { op, .. } => {
+                    let op_name = op.inner.to_string();
+
+                    format!(
+                        "hint: operator '{}' expects operands of type {}",
+                        op_name, e.expected
+                    )
+                }
+                Expr::UnaryOp { op, .. } => {
+                    format!(
+                        "hint: unary operator '{}' expects operand of type {}",
+                        op.inner, e.expected
+                    )
+                }
+                Expr::FunctionCall { .. } => {
+                    panic!("function args are dynamic so this code should never run")
+                }
+                Expr::ArrayAccess { left, right } => match &right.inner {
+                    ArrayIndex::SingleIndex(idx) => {
+                        if e.origin_expr.span == idx.span {
                             format!(
-                                "hint: operator '{}' expects operands of type {}",
-                                op_name, e.expected
+                                "hint: the right side of this subscript operation must be of type {}",
+                                e.expected
                             )
-                        }
-                        Expr::UnaryOp { op, .. } => {
+                        } else if e.origin_expr.span == left.span {
                             format!(
-                                "hint: unary operator '{}' expects operand of type {}",
-                                op.inner, e.expected
+                                "hint: the left side of this subscript operation must be of type {}",
+                                e.expected
                             )
+                        } else {
+                            panic!("type error context does not match any sub-expression")
                         }
-                        Expr::FunctionCall { .. } => {
-                            panic!("function args are dynamic so this code should never run")
+                    }
+                    ArrayIndex::Slice { start, end, .. } => {
+                        if Some(e.origin_expr.span) == start.as_ref().map(|s| s.span)
+                            || Some(e.origin_expr.span) == end.as_ref().map(|s| s.span)
+                        {
+                            format!(
+                                "hint: indices of this slice operation must be of type {}",
+                                e.expected
+                            )
+                        } else if e.origin_expr.span == left.span {
+                            format!(
+                                "hint: the left side of this slice operation must be of type {}",
+                                e.expected
+                            )
+                        } else {
+                            panic!("type error context does not match any sub-expression")
                         }
-                        Expr::ArrayAccess { left, right } => match &right.inner {
-                            ArrayIndex::SingleIndex(idx) => {
-                                if e.origin_expr.span == idx.span {
-                                    format!(
-                                        "hint: the right side of this subscript operation must be of type {}",
-                                        e.expected
-                                    )
-                                } else if e.origin_expr.span == left.span {
-                                    format!(
-                                        "hint: the left side of this subscript operation must be of type {}",
-                                        e.expected
-                                    )
-                                } else {
-                                    panic!("type error context does not match any sub-expression")
-                                }
-                            }
-                            ArrayIndex::Slice { start, end, .. } => {
-                                if Some(e.origin_expr.span) == start.as_ref().map(|s| s.span)
-                                    || Some(e.origin_expr.span) == end.as_ref().map(|s| s.span)
-                                {
-                                    format!(
-                                        "hint: indices of this slice operation must be of type {}",
-                                        e.expected
-                                    )
-                                } else if e.origin_expr.span == left.span {
-                                    format!(
-                                        "hint: the left side of this slice operation must be of type {}",
-                                        e.expected
-                                    )
-                                } else {
-                                    panic!("type error context does not match any sub-expression")
-                                }
-                            }
-                        },
-                        _ => panic!("type error context does not match any sub-expression"),
-                                            }
-                        TypeErrorContext::IfStatementCond(_) => format!("hint: the condition for this if statement must be of type {}", e.expected),
-                        TypeErrorContext::WhileStatementCond(_) => format!("hint: the condition for this while loop must be of type {}", e.expected),
-                        TypeErrorContext::ForStatementRange(_) => format!("hint: the bounds for this for loop must be of type {}", e.expected),
-                        TypeErrorContext::Other => todo!(),
-                    }),
-                ])
-                .finish();
+                    }
+                },
+                _ => panic!("type error context does not match any sub-expression"),
+            },
+            TypeErrorContext::IfStatementCond(_) => format!(
+                "hint: the condition for this if statement must be of type {}",
+                e.expected
+            ),
+            TypeErrorContext::WhileStatementCond(_) => format!(
+                "hint: the condition for this while loop must be of type {}",
+                e.expected
+            ),
+            TypeErrorContext::ForStatementRange(_) => format!(
+                "hint: the bounds for this for loop must be of type {}",
+                e.expected
+            ),
+            TypeErrorContext::Other => todo!(),
+        };
+
+        let hint = Label::new(hint_span)
+            .with_color(Color::Yellow)
+            .with_message(hint_message);
+
+        let report = Report::build(
+            ReportKind::Error,
+            (
+                file_name.clone(),
+                e.origin_expr.span.start.bytes..e.origin_expr.span.end.bytes,
+            ),
+        )
+        .with_message(format!(
+            "Type mismatch: expected {}, found {}",
+            e.expected, e.found
+        ))
+        .with_labels([
+            Label::new((
+                file_name.clone(),
+                e.origin_expr.span.start.bytes..e.origin_expr.span.end.bytes,
+            ))
+            .with_message(format!(
+                "expected type {}, but this expression results in type {}",
+                e.expected, e.found
+            ))
+            .with_color(Color::Red),
+            hint,
+        ])
+        .finish();
 
         report.print(sources([(file_name.clone(), src)])).unwrap();
     }
