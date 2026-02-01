64f4a1bec7f61033edb82c95e572df02de53d501
Ashley Hawkins <awhawkins@proton.me>
2026-02-01 15:41:45 +0000

    Type safety for correct broadcast events

--- web-ui/src/types.ts
+++ web-ui/src/types.ts
@@ -1,4 +1,29 @@
-import type { ComponentContainer, JsonValue } from "golden-layout";
+import type { ComponentContainer, EventHub, JsonValue } from "golden-layout";
+
+export const enum UserBroadcastType {
+  parserOutput,
+  envVarsUpdate
+}
+
+interface ParserOutputBroadcastData {
+  type: UserBroadcastType.parserOutput,
+  output: string
+}
+
+interface EnvVarsUpdateBroadcastData {
+  type: UserBroadcastType.envVarsUpdate,
+  envVars: { key: string, value: string }[]
+}
+
+export type UserBroadcastData = ParserOutputBroadcastData | EnvVarsUpdateBroadcastData;
+
+export function emitUserBroadcast (eventHub: EventHub, data: UserBroadcastData) {
+  eventHub.emit('userBroadcast', data);
+}
+
+export function onUserBroadcast(eventHub: EventHub, callback: (data: UserBroadcastData) => void) {
+  eventHub.on('userBroadcast', <any>(callback));
+}
 
 export interface IGoldenLayoutProps {
   glContainer: ComponentContainer,
